5084d06bdb538041566697fb0b2babe3eb801732
Søren Godbersen <sorengod@gmail.com>
2023-02-05 18:11:15 +0100

    Deleting test files

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_excel("Spot_Prices.xlsx")
-df = df.pivot_table(index='HourDK', columns='GridFlow', values='ImportCapacity')
-print(df)
--- testt.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-
-df = pd.read_parquet('Spot_Prices.parq')
-print(df)
