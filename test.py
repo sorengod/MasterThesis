529ced848c047628c06421b2e0d5b4c2093e0f30
Søren Godbersen <sorengod@gmail.com>
2023-03-22 00:06:21 +0100

    Updated Thesis

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-import pandas as pd 
 
-df = pd.read_pickle("Data/MeanWindPower1h_Anholdt.pkl")
-print(df)
+
+def naive_relu(x):
+    assert len(x.shape) == 2
+    x = x.copy()
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            x[i, j] = max(x[i, j], 0)
+    return x
+
+print(naive_relu(2))
